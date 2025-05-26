caff0c65e637d999f417a61fad003631ec4ff9ee
Victor Mercklé <victor.merckle@gmail.com>
2025-05-26 12:34:30 +0200

    day2 **

--- day2/main.py
+++ day2/main.py
@@ -6,12 +6,11 @@ def parse(filename):
             ll.append([int(x) for x in line])
     return ll
 
-filename = "demo_input.csv"
-filename = "input.csv" # 1000 lines of 8 elements
-ll = parse(filename)
-#print(f"{len(ll)} lines of {max([len(l) for l in ll])} elements")
 
-def safe(l):
+
+# obviously inefficient pbdampener, no need to read the list again
+# same complexity however
+def safe(l, pbdampener=False):
     if len(l) <= 1:
         return True
     # sequence must be strictly monotonous,
@@ -19,14 +18,35 @@ def safe(l):
 
     up = l[0] < l[1] # true if increasing
     lastx = l[0]
-    for x in l[1:]:
+    for i in range(1, len(l)):
+        x = l[i]
         d = abs(x - lastx)
         if d == 0 or d > 3: # integers
-            return False
+            if not pbdampener:
+                return False
+            second = list(l)
+            second.pop(i)
+            l.pop(i-1)
+            return safe(l) or safe(second)
         if up != (lastx < x): # necessary ()
-            return False
+            if not pbdampener:
+                return False
+            second = list(l)
+            second.pop(i)
+            l.pop(i-1)
+            if i == 2:
+                third= list(l)
+                third.pop(0)
+                return safe(l) or safe(second) or safe(third)
+            return safe(l) or safe(second)
         lastx = x
     return True
 
+filename = "demo_input.csv"
+filename = "input.csv" # 1000 lines of 8 elements
+ll = parse(filename)
+
+#print(f"{len(ll)} lines of {max([len(l) for l in ll])} elements")
 #print(safe(ll[-1]))
 print(sum([safe(l) for l in ll]))
+print(sum([safe(l, pbdampener=True) for l in ll]))
---
+++ day2/notes.md
@@ -0,0 +1,11 @@
+When a problem is encountered between two numbers, one of them as to be removed. 
+
+--> wrong:
+
+4 5 4 3 2 1
+
+i=0, no problems
+i=1, if distance is too great, remove i=0 or i=1.
+up or down is defined
+i=2, if dist is too great, try remove i=1 or i=2
+if we're going wrong way, can try to remove i=0 as well.
